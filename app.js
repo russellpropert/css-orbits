ce898497782451034b6f0027ad48d2f7906ccb21
Russell Propert <russellpropert@gmail.com>
2022-06-27 03:28:22 -0400

    Adding static directory to Express and type: module to package.json.

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 import express from 'express';
 
 const app = express();
+const dir = process.cwd();
 
-app.listen(3000);
-
-app.get('/'), (req, res) => {
-  res.sendFile(`${__dirname}/orbits.html`);
+const port = 3000;
+const options = {
+  dotfiles: "ignore",
+  redirect: false
 }
+
+app.use('/', express.static(dir, options));
+
+app.get('/', (req, res) => {
+  res.sendFile(`${dir}/orbits.html`);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
